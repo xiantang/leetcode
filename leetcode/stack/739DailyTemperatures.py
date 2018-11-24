5513aebf8c6c943216343b7368208fd2edad71a6
dd <986494553@qq.com>
2018-11-24 11:12:59 +0800

    73

---
+++ leetcode/stack/739DailyTemperatures.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def dailyTemperatures(self, T):
+        """
+        :type T: List[int]
+        :rtype: List[int]
+        """
+        stack = []
+        ans = [0] * len(T)
+
+        # 遍历T
+        for i, t in enumerate(T):
+            # 如果栈为空就加入栈中
+            # 遍历到每个节点 都为之前的节点更新
+            while stack and T[stack[-1]] < t:
+                cur = stack.pop()
+                ans[cur] = i - cur
+            stack.append(i)
+        return ans
+Solution().dailyTemperatures([73,74,75,71,69,72,76,73])
