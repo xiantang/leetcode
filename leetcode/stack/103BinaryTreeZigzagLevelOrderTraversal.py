f1012fb6c99b59b02f0ba263951833bb699a814a
dd <986494553@qq.com>
2018-11-24 15:14:36 +0800

    103

---
+++ leetcode/stack/103BinaryTreeZigzagLevelOrderTraversal.py
@@ -0,0 +1,27 @@
+import collections
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: T
+        reeNode
+        :rtype: List[List[int]]
+        """
+        stack = []
+
+        stack.append(root)
+        res = []
+        tem = []
+        flag = 1
+        while stack:
+            for i in range(len(stack)):
+                node = stack.pop(0)
+                tem.append(node.val)
+
+                if node.left:
+                    stack.append(node.left)
+                elif node.right:
+                    stack.append(node.right)
+            res.append(tem[::flag])
+            flag = -flag
+            tem = []
+        return res
