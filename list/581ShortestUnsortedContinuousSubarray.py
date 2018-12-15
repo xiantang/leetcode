f9189b15c17a5920e62b62cd6c0ebeef300154d5
xiantang <986494553@qq.com>
2018-12-15 14:47:40 +0800

    581

---
+++ list/581ShortestUnsortedContinuousSubarray.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def findUnsortedSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        m = nums[0]
+        end = 0
+        for i in range(1, len(nums)):
+            if nums[i] >= m:
+                m = nums[i]
+            else:
+                end = i
+
+        m = nums[-1]
+        start = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] <= m:
+                m = nums[i]
+            else:
+                start = i
+
+        if start == end:
+            return 0
+
+        return max(end - start + 1, 0)
