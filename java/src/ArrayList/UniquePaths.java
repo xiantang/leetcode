6b9a24034134c9ddacb493a861407b14d9b641d2
dd <986494553@qq.com>
2018-12-29 16:46:50 +0800

    62

---
+++ java/src/ArrayList/UniquePaths.java
@@ -0,0 +1,18 @@
+package ArrayList;
+
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int [][] res=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(i==0||j==0)
+                    res[i][j]=1;
+                else{
+                    res[i][j] = res[i-1][j]+res[i][j-1];
+                }
+
+            }
+        }
+        return res[m-1][n-1];
+    }
+}
