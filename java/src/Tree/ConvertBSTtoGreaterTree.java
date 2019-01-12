31cb29e94d52452c9c7a539917c77eff2d7835d6
xiantang <986494553@qq.com>
2019-01-12 19:48:49 +0800

    538

---
+++ java/src/Tree/ConvertBSTtoGreaterTree.java
@@ -0,0 +1,14 @@
+package Tree;
+
+public class ConvertBSTtoGreaterTree {
+    private int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if(root!=null){
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
