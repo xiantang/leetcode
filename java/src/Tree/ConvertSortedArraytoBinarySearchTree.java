9caa9dcaef7add4ce87316d528690320b7fcae7f
xiantang <986494553@qq.com>
2019-01-19 13:57:11 +0800

    Convert Sorted Array to Binary Search Tree

--- java/src/Tree/ConvertSortedArraytoBinarySearchTree.java
+++ java/src/Tree/ConvertSortedArraytoBinarySearchTree.java
@@ -2,35 +2,32 @@ package Tree;
 
 public class ConvertSortedArraytoBinarySearchTree {
     public TreeNode sortedArrayToBST(int[] nums) {
-        if(nums.length==0){
-            return null;
-        }
-        int mid = nums.length/2;
-        TreeNode root = new TreeNode(nums[mid]);
-        for(int i=0;i<mid;i++){
-            root = buildTree(root,nums[i]);
-        }
-        for(int i=mid+1;i<nums.length;i++){
-            root = buildTree(root,nums[i]);
-        }
+
+        TreeNode root = null;
+
+        root = buildTree(null,nums,0,nums.length);
+
         return root;
     }
 
-    public  TreeNode buildTree(TreeNode root,int num){
-        if(root==null){
-            return new TreeNode(num);
-        }
-        if(num>root.val)
-            root.right = buildTree(root.right,num);
-        if(num<root.val)
 
-            root.left = buildTree(root.left,num);
+    public  TreeNode buildTree(TreeNode root,int[] nums,int low,int hi){
+        if(low >=hi ){
+            return null;
+        }
+        int midIndex = (low + hi)/2;
+        int  mid= nums[midIndex];
+        if(root == null){
+            root = new TreeNode(mid);
+        }
+        // root.val = mid;
+        root.left = buildTree(root.left,nums,low,midIndex);
+        root.right = buildTree(root.right,nums,midIndex+1,hi);
         return root;
     }
 
     public static void main(String[] args) {
-        new ConvertSortedArraytoBinarySearchTree().sortedArrayToBST(new int[] {
-                0,1,2,3,4,5
+        new ConvertSortedArraytoBinarySearchTree().sortedArrayToBST(new int[] {-10,-3,0,5,9
         });
     }
 }
