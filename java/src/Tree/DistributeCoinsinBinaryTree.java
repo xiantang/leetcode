96cee23d291a92c7b3523112a9c672f79fc0010c
dd <986494553@qq.com>
2019-02-23 13:46:32 +0800

    979. Distribute Coins in Binary Tree

---
+++ java/src/Tree/DistributeCoinsinBinaryTree.java
@@ -0,0 +1,19 @@
+package Tree;
+
+public class DistributeCoinsinBinaryTree {
+    int ans;
+    public int distributeCoins(TreeNode root) {
+        ans = 0;
+        dfs(root);
+        return ans;
+    }
+    public int dfs(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        ans += Math.abs(left)+Math.abs(right);
+        return left+right+root.val-1;
+    }
+}
