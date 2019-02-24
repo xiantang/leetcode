1953decbf1fe89767e83dd53430826a422942700
dd <986494553@qq.com>
2019-02-24 13:47:54 +0800

    515. Find Largest Value in Each Tree Row

--- java/src/Tree/ConstructBinaryTreefromPreorderandPostorderTraversal.java
+++ java/src/Tree/ConstructBinaryTreefromPreorderandPostorderTraversal.java
@@ -23,4 +23,6 @@ public class ConstructBinaryTreefromPreorderandPostorderTraversal {
         return root;
 
     }
+
+
 }
--- java/src/Tree/FindLargestValueInEachTreeRow.java
+++ java/src/Tree/FindLargestValueInEachTreeRow.java
@@ -1,32 +1,30 @@
 package Tree;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
-public class FindBottomLeftTreeValue {
-    public int findBottomLeftValue(TreeNode root) {
+public class FindLargestValueInEachTreeRow {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
+        if(root==null){
+            return res;
+        }
         queue.offer(root);
-        List<List<TreeNode>> res = new ArrayList<>();
+
         while (queue.size() != 0) {
             int size = queue.size();
-            List<TreeNode> list = new ArrayList<>();
+            List<Integer> list = new ArrayList<>();
             for (int i = 0; i < size; i++) {
                 TreeNode node =queue.poll();
-                list.add(node);
+                list.add(node.val);
                 if (node.left!=null)
                     queue.offer(node.left);
                 if (node.right!=null)
                     queue.offer(node.right);
-
             }
-            res.add(list);
-
+            int id = Collections.max(list);
+            res.add(id);
         }
-        return  res.get(res.size()-1).get(0).val;
-
-
-
-
+        return res;
     }
 }
