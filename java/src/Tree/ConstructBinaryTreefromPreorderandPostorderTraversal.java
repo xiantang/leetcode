4f4eb1f4a55708c1fc5e6e25acebb772dae544f1
dd <986494553@qq.com>
2019-02-24 13:23:03 +0800

    889. Construct Binary Tree from Preorder and Postorder Traversal

---
+++ java/src/Tree/ConstructBinaryTreefromPreorderandPostorderTraversal.java
@@ -0,0 +1,26 @@
+package Tree;
+
+import java.util.Arrays;
+
+public class ConstructBinaryTreefromPreorderandPostorderTraversal {
+    public TreeNode constructFromPrePost(int[] pre, int[] post) {
+        if (pre.length == 0){
+            return null;
+        }
+        TreeNode root = new TreeNode(pre[0]);
+        if(pre.length == 1){
+            return root;
+        }
+        int indexPost = 0;
+        for(int i=0;i<post.length;i++){
+            if (post[i] == pre[1]){
+                indexPost = i+1;
+            }
+        }
+
+        root.left = constructFromPrePost(Arrays.copyOfRange(pre,1,indexPost+1),Arrays.copyOfRange(post,0,indexPost));
+        root.right = constructFromPrePost(Arrays.copyOfRange(pre,1+indexPost,pre.length),Arrays.copyOfRange(post,indexPost,pre.length));
+        return root;
+
+    }
+}
