8ae9a65cda7bfbf3bb8f6dc4cd0b2fa170b1f31d
dd <986494553@qq.com>
2019-03-02 13:30:15 +0800

    623

--- java/src/Tree/623.add-one-row-to-tree.java
+++ java/src/Tree/623.add-one-row-to-tree.java
@@ -94,7 +94,37 @@
  */
 class Solution {
     public TreeNode addOneRow(TreeNode root, int v, int d) {
-        
+        if(d == 1){
+            TreeNode node = new TreeNode(v);
+            node.left = root;
+            return node;
+        }
+        Queue < TreeNode > queue = new LinkedList < > ();
+        queue.add(root);
+        int depth = 1;
+        while(depth<d-1){
+            Queue < TreeNode > temp = new LinkedList < > ();
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.remove();
+                if (node.left != null) temp.add(node.left);
+                if (node.right != null) temp.add(node.right);
+            }
+            queue= temp;
+            depth += 1;
+        }
+        while(!queue.isEmpty()){
+            TreeNode node =queue.remove();
+            TreeNode newNode = new TreeNode(v);
+            TreeNode right = node.right;
+            newNode.right = right;
+            node.right = newNode;
+            TreeNode nwNode = new TreeNode(v);
+            TreeNode left = node.left;
+            nwNode.left = left;
+            node.left = nwNode;
+            
+        }
+        return root;
     }
 }
 
