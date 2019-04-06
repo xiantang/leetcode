e8a290168e8eb27951bc855221c5a1ed88ab227a
xiantang <986494553@qq.com>
2019-04-06 14:28:02 +0800

    225. Implement Stack using Queues

---
+++ java/src/stack/225.implement-stack-using-queues.java
@@ -0,0 +1,103 @@
+/*
+ * @lc app=leetcode id=225 lang=java
+ *
+ * [225] Implement Stack using Queues
+ *
+ * https://leetcode.com/problems/implement-stack-using-queues/description/
+ *
+ * algorithms
+ * Easy (38.52%)
+ * Total Accepted:    123.8K
+ * Total Submissions: 321.3K
+ * Testcase Example:  '["MyStack","push","push","top","pop","empty"]\n[[],[1],[2],[],[],[]]'
+ *
+ * Implement the following operations of a stack using queues.
+ * 
+ * 
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * empty() -- Return whether the stack is empty.
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * MyStack stack = new MyStack();
+ * 
+ * stack.push(1);
+ * stack.push(2);  
+ * stack.top();   // returns 2
+ * stack.pop();   // returns 2
+ * stack.empty(); // returns false
+ * 
+ * Notes:
+ * 
+ * 
+ * You must use only standard operations of a queue -- which means only push to
+ * back, peek/pop from front, size, and is empty operations are valid.
+ * Depending on your language, queue may not be supported natively. You may
+ * simulate a queue by using a list or deque (double-ended queue), as long as
+ * you use only standard operations of a queue.
+ * You may assume that all operations are valid (for example, no pop or top
+ * operations will be called on an empty stack).
+ * 
+ * 
+ */
+class MyStack {
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+    private int top = -1;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queue1=  new LinkedList();
+        queue2 = new LinkedList();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue1.add(x);
+        top = x;
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        while(queue1.size()>1){
+            queue2.add(queue1.poll());
+        }
+        int res = queue1.poll();
+        while(queue2.size()!=0){
+            push(queue2.poll());
+        }
+        return res;
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return top;
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+
