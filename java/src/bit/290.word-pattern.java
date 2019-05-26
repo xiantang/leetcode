2176753a18aa5147081ee8d62776a3b3b48b92cf
xiantang <986494553@qq.com>
2019-05-26 16:16:51 +0800

    290. Word Pattern

---
+++ java/src/bit/290.word-pattern.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=290 lang=java
+ *
+ * [290] Word Pattern
+ */
+class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (words.length != pattern.length())
+            return false;
+        Map index = new HashMap();
+        for (Integer i=0; i<words.length; ++i)
+            if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+                return false;
+        return true;
+    }
+}
+
